92876b965d36d515b143a0c94120d3acd7ea6588
jonsey247 <jacobfilmjones@hotmail.com>
2018-11-13 18:58:05 +0000

    table added //TODO: get real values and try to break table out into its own component

--- pages/about-the-home.vue
+++ pages/about-the-home.vue
@@ -13,17 +13,37 @@
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
-          <small>
-            <em>&mdash;{{ labels }}</em>
-          </small>
+          <div id="components-demo">
+            <testTable/>
+          </div>
         </footer>
       </blockquote>
     </v-flex>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      hide-actions
+      class="elevation-1"
+    >
+      <template 
+        slot="items" 
+        slot-scope="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-left">{{ props.item.calories }}</td>
+        <td class="text-xs-left">{{ props.item.fat }}</td>
+        <td class="text-xs-left">{{ props.item.carbs }}</td>
+        <td class="text-xs-left">{{ props.item.protein }}</td>
+        <td class="text-xs-left">{{ props.item.iron }}</td>
+      </template>
+    </v-data-table>
   </v-layout>
 </template>
 
 <script>
 import data from '../assets/Desktop/aboutthehome.report.json'
+import testTable from '../components/testTable.js'
+import Vue from 'vue'
+new Vue({ el: '#components-demo' })
 export default {
   data() {
     return {
@@ -41,6 +61,57 @@ export default {
         data.categories.pwa.score * 100,
         data.categories.accessibility.score * 100,
         data.categories.seo.score * 100
+      ],
+      headers: [
+        {
+          text: 'Lighthouse Report',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'About The Home', value: 'calories' },
+        { text: 'About The Policy Holder', value: 'fat' },
+        { text: 'High Impact Questions', value: 'carbs' },
+        { text: 'Home Page', value: 'protein' },
+        { text: 'Results Page', value: 'iron' }
+      ],
+      desserts: [
+        {
+          value: false,
+          name: 'Performance',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%'
+        },
+        {
+          value: false,
+          name: 'Progressive Web App',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%'
+        },
+        {
+          value: false,
+          name: 'Accessibility',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%'
+        },
+        {
+          value: false,
+          name: 'SEO',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%'
+        }
       ]
     }
   }
