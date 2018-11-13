986ec17b3f54ea188b9ee928367cd28d5bbe014a
jonsey247 <jacobfilmjones@hotmail.com>
2018-11-13 16:58:06 +0000

    implament file upload service //TODO: only works for images, needs to be changed to json files

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,161 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center>
-    <v-flex
-      xs12
-      sm8
-      md6>
-      <div class="text-xs-center">
-        <logo/>
-        <vuetify-logo/>
+  <div>
+    <form
+      v-if="isInitial || isSaving"
+      enctype="multipart/form-data" 
+      novalidate>
+      <h1>Upload images</h1>
+      <div class="dropbox">
+        <input
+          :name="uploadFieldName" 
+          :disabled="isSaving"
+          accept="image/*"
+          type="file"
+          class="input-file"
+          multiple
+          @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length">
+        <p v-if="isInitial">
+          Drag your file(s) here to begin<br> or click to browse
+        </p>
+        <p v-if="isSaving">
+          Uploading {{ fileCount }} files...
+        </p>
       </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a
-            href="https://vuetifyjs.com"
-            target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank">Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+    </form>
+    <!--SUCCESS-->
+    <div v-if="isSuccess">
+      <h2>Uploaded {{ uploadedFiles.length }} file(s) successfully.</h2>
+      <p>
+        <a 
+          href="javascript:void(0)" 
+          @click="reset()">Upload again</a>
+      </p>
+      <ul class="list-unstyled">
+        <li 
+          v-for="item in uploadedFiles" 
+          :key="item.url">
+          <img 
+            :src="item.url"
+            :alt="item.originalName"
+            class="img-responsive img-thumbnail">
+        </li>
+      </ul>
+    </div>
+    <!--FAILED-->
+    <div v-if="isFailed">
+      <h2>Uploaded failed.</h2>
+      <p>
+        <a 
+          href="javascript:void(0)" 
+          @click="reset()">Try again</a>
+      </p>
+      <pre>{{ uploadError }}</pre>
+    </div>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+// import { upload } from '../store/file-upload.fake.service' // fake service
+import { upload } from '../store/file-upload.service.js'
+
+const STATUS_INITIAL = 0,
+  STATUS_SAVING = 1,
+  STATUS_SUCCESS = 2,
+  STATUS_FAILED = 3
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: 'photos',
+      fileCount: ''
+    }
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED
+    }
+  },
+  mounted() {
+    this.reset()
+  },
+  methods: {
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL
+      this.uploadedFiles = []
+      this.uploadError = null
+    },
+    save(formData) {
+      // upload data to the server
+      this.currentStatus = STATUS_SAVING
+
+      upload(formData)
+        .then(x => {
+          this.uploadedFiles = [].concat(x)
+          this.currentStatus = STATUS_SUCCESS
+        })
+        .catch(err => {
+          this.uploadError = err.response
+          this.currentStatus = STATUS_FAILED
+        })
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData()
+
+      if (!fileList.length) return
+
+      // append the files to FormData
+      Array.from(Array(fileList.length).keys()).map(x => {
+        formData.append(fieldName, fileList[x], fileList[x].name)
+      })
+
+      // save it
+      this.save(formData)
+    }
   }
 }
 </script>
+
+<style>
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background: lightcyan;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px; /* minimum height */
+  position: relative;
+  cursor: pointer;
+}
+
+.input-file {
+  opacity: 0; /* invisible but it's there! */
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  cursor: pointer;
+}
+
+.dropbox:hover {
+  background: lightblue; /* when mouse over to the drop zone, change color */
+}
+
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
+</style>
