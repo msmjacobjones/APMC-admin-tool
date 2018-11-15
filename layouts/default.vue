6bc5367131dffb4028767be549de3f0a4900c379
jonsey247 <jacobfilmjones@hotmail.com>
2018-11-15 09:47:59 +0000

    fix for: Error while initializing app DOMException: Failed to execute 'appendChild' on 'Node':

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,22 +7,24 @@
       fixed
       app
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon" />
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      <no-ssr>
+        <v-list>
+          <v-list-tile
+            v-for="(item, i) in items"
+            :to="item.to"
+            :key="i"
+            router
+            exact
+          >
+            <v-list-tile-action>
+              <v-icon v-html="item.icon" />
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title" />
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </no-ssr>
     </v-navigation-drawer>
     <v-toolbar
       :clipped-left="clipped"
